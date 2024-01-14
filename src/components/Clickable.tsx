ff41e5be44949cc7516fd6f679b23fce00d55967
Allan Hortle <allanhortle@gmail.com>
2024-01-14 12:07:00 +1100

    Upgrade next

--- src/components/Clickable.tsx
+++ src/components/Clickable.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import NextLink from 'next/link';
-import styled from 'styled-components';
 
-const StyledLink = styled.a`
-    color: inherit;
-    text-decoration: none;
-
-    &:visited {
-        color: inherit;
-    }
-`;
-
-export default function Link({href, children}: {href: string; children: React.ReactNode}) {
+export default function Clickable({href, children}: {href: string; children: React.ReactNode}) {
     return (
         <NextLink href={href} passHref>
-            <StyledLink>{children}</StyledLink>
+            {children}
         </NextLink>
     );
 }
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import NextLink from 'next/link';
-import styled from 'styled-components';
-
-const StyledLink = styled.a`
-    color: ${(p) => p.theme.colors.blue};
-
-    &:visited {
-        color: ${(p) => p.theme.colors.purple};
-    }
-`;
 
 export default function Link({href, children}: {href: string; children: React.ReactNode}) {
     return (
         <NextLink href={href} passHref>
-            <StyledLink>{children}</StyledLink>
+            {children}
         </NextLink>
     );
 }
